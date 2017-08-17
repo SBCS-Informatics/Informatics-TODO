b31c594522046ed6f51f5ed6c2b7dfc13f9978a1
adrlar <adr.lar@me.com>
2017-08-17 15:43:33 +0100

    finished logging

--- scripts/monitoring/hive_deduping.py
+++ scripts/monitoring/hive_deduping.py
@@ -3,22 +3,15 @@ import sys, time, argparse, os, hashlib
 from pwd import getpwuid
 from multiprocessing import Pool
 
+#DEFAULTS
 NUM_THREADS=8
 WRITE_HASH_TO_FILE=1
 
-date_string = time.strftime("%Y_%m_%d",time.localtime())
-
-##used for binary test. not fool proof but good enough for this use case
-##taken from https://stackoverflow.com/questions/898669/how-can-i-detect-if-a-file-is-binary-non-text-in-python
-textchars = bytearray({7,8,9,10,12,13,27} | set(range(0x20, 0x100)) - {0x7f})
-def is_binary(filename):
-    file_part = open(filename, 'rb').read(1024)
-    return bool(file_part.translate(None, textchars))
-
-##main function
+### MAIN ###
 def main():
-    #time start
+    #time start and date
     t_zero = time.time()
+    date_string = time.strftime("%Y_%m_%d",time.localtime())
 
     #usage
     if len(sys.argv)!=2:
@@ -31,34 +24,59 @@ def main():
     else:
         exit("%s directory does not exist"%sys.argv[1])
     
-    list_of_files = find_all_files(root)
+    #List all files and directories under the root directory provided
+    list_of_files, list_of_dirs = find_all_files(root)
+    print len(list_of_dirs), "directories"
     print len(list_of_files), "files"
 
 
-    #for f in list_of_files:
-    #    if is_binary(f):
-    #        print f, "binary"
-    #    else:
-    #        print f, "NONbinary"
+    ### LOOK FOR DIRS WITH MORE THAN 100 ENTRIES IN THEM ###
+    list_of_dirs_to_tar = list()
+    nested_to_tar=dict()
+    users_tar=list()
+    for d in sorted(list_of_dirs):
+        if len(os.listdir(d))>100:
+            for dd in list_of_dirs_to_tar:
+                if dd in d:
+                    if dd in nested_to_tar.keys():
+                        nested_to_tar[dd].append(d)
+                    else:
+                        nested_to_tar[dd]=[d]
+            user = get_uid(d)
+            if user not in users_tar:
+                users_tar.append(user)
+            list_of_dirs_to_tar.append(d)
+
+    for key in nested_to_tar.keys():
+        for nested_dir in nested_to_tar[key]:
+            list_of_dirs_to_tar.remove(nested_dir)
+
+    #print list_of_dirs_to_tar
+    #print nested_to_tar
+
     
-    
-    #list of hash values, the file and its file size
+    #calculate checksum values and file size for all files
     #[[hash, filesize, filename], ...]
+    t_hash_zero = time.time()
     list_of_hashes = multithread_run(hash_file, list_of_files)
-    
+    t_hash = time.time()-t_hash_zero
+    print round(t_hash, 2), "s hash time"
+
     #file_dict will keep a hash and its corresponding files
     file_dict=dict()
     #files_to_compress will keep a list of all files that should be compressed: 
     #>100MB and nonbinary as defined by function is_binary
     files_to_compress=list()
     total_uncompressed_size=0 
-    
-    print round(time.time()-t_zero), "seconds"
+    users_dedup=list()
+    users_compress=list()
+
     
     
     #Go through all the hash values and see if there are duplicates
     #Also check if the files are binary and if not add to list
     for f in list_of_hashes:
+        user=get_uid(f[2])
         if f[0] not in file_dict.keys():
             #if this hash is not already in the dict
             #add the hash, point to the filesize and the filename f[1:]
@@ -68,7 +86,10 @@ def main():
             #only need to add its filename f[2], 
             #already have the size
             file_dict[f[0]].append(f[2])
-        
+            #Going to store user too
+            if user not in users_dedup:
+                users_dedup.append(user)
+
         #Going to check file size and if file is binary in the same loop
         file_size=float(f[1])
         if file_size > 100000000: #Filesize > than 100MiB
@@ -76,7 +97,10 @@ def main():
             if not is_binary(f[2]):
                 #add to list
                 total_uncompressed_size+=file_size
-                files_to_compress.append([get_uid(f[2]), f[2]])
+                files_to_compress.append([user, f[2]])
+                #add user to list
+                if user not in users_compress:
+                    users_compress.append(user)
 
     #print files_to_compress, file_size/1000000000
     print "Approximate space that could be saved by compressing {} files: {} GiB".format(len(files_to_compress),total_uncompressed_size/1000000000*0.6)
@@ -90,49 +114,89 @@ def main():
     total_size,reduced_size,num_to_dedup = size_of_dups(duplicated_files)
     print "Possible space to saved by deduping {} files: {} GiB".format(num_to_dedup, total_size/1000000000 - reduced_size/1000000000)
     
-    print round(time.time()-t_zero), "seconds"
     tmp_file_list = find_tmp_files(list_of_files)
     print len(tmp_file_list), "tmp files"
-    print round(time.time()-t_zero), "seconds"
 
     ###REPORT###
     #Open logfiles to write to
 
     if len(duplicated_files)>0:
         logfile = open("hive_deduping_duplicatedfiles_"+date_string+".log", "w")
+        logfile_users=dict()
+        for user in users_dedup:
+            logfile_user="hive_deduping_duplicatedfiles_"+date_string+".log."+user
+            logfile_users[user]=open(logfile_user, "w")
+
         for key in duplicated_files.keys():
-            users = list() 
+            users_key = list() 
             for dupfile in sorted(duplicated_files[key][1:]):
                 user = get_uid(dupfile)
-                logfile.write("{}\t{}\t{}\n".format(key, user, dupfile))
-
-                if user not in users:
-                    users.append(user)
-                logfile_user="hive_deduping_duplicatedfiles_"+date_string+".log."+user
-                if os.path.exists(logfile_user):
-                    append_write = 'a' # append if already exists
-                else:
-                    append_write = 'w' # make a new file if not
-                logfile_user = open(logfile_user, append_write)
-                logfile_user.write("{}\t{}\t{}\n".format(key, user, dupfile))
-                logfile_user.close()
-            for user in users:
-                logfile_user="hive_deduping_duplicatedfiles_"+date_string+".log."+user
-                logfile_user = open(logfile_user, "a")
-                logfile_user.write("\n")
-                logfile_user.close()
-                
+                if user not in users_key:
+                    users_key.append(user)
+                logfile.write("{}\t{}\t{}\t{}\n".format(key, user, duplicated_files[key][0], dupfile))
+            
             logfile.write("\n")
+            for user_key in users_key:
+                for dupfile in sorted(duplicated_files[key][1:]):
+                    user = get_uid(dupfile)
+                    logfile_users[user_key].write("{}\t{}\t{}\n".format(key, duplicated_files[key][0], dupfile))
+                logfile_users[user_key].write("\n")
+                
+        #Close logfiles
         logfile.close() 
+        for user in users_dedup:
+            logfile_users[user].close()
 
     if len(files_to_compress)>0:
         logfile = open("hive_deduping_filestocompress_"+date_string+".log", "w")
-        logfile_user = open("hive_deduping_filestocompress_"+date_string+".log."+user, "w")
+        logfile_users=dict()
+        for user in users_compress:
+            logfile_user="hive_deduping_filestocompress_"+date_string+".log."+user
+            logfile_users[user]=open(logfile_user, "w")
         
-        logfile.write(str(files_to_compress))
+        for f in files_to_compress:
+            logfile.write("{}\t{}\n".format(f[0],f[1]))
+            logfile_users[f[0]].write("{}\n".format(f[1]))
+        
+        #Close logfiles
+        for user in users_compress:
+            logfile_users[user].close()
+        logfile.close()
+
+    if len(list_of_dirs_to_tar)>0:
+        nested_to_tar
+        logfile = open("hive_deduping_dirstotar_"+date_string+".log", "w")
+        for user in users_tar:
+            logfile_user="hive_deduping_dirstotar_"+date_string+".log."+user
+            logfile_users[user]=open(logfile_user, "w")
+        for d in list_of_dirs_to_tar:
+            user = get_uid(d)
+            logfile.write("{}\t{}\n".format(user, d))
+            logfile_users[user].write("{}\n".format(d))
+            if d in nested_to_tar.keys():
+                for dd in nested_to_tar[d]:
+                    user_nested = get_uid(dd)
+                    logfile.write("\t{}\t{}\n".format(user_nested, dd))
+                    logfile_users[user].write("\t{}\n".format(dd))
+                    if not user==user_nested:
+                        logfile_users[user_nested].write("{}\t{}\n".format(user_nested, dd))
+
+        #Close logfiles
+        for user in users_tar:
+            logfile_users[user].close()
         logfile.close()
 
 
+
+
+### FUNCTIONS ###
+
+##used for binary test. not fool proof but good enough for this use case
+##taken from https://stackoverflow.com/questions/898669/how-can-i-detect-if-a-file-is-binary-non-text-in-python
+textchars = bytearray({7,8,9,10,12,13,27} | set(range(0x20, 0x100)) - {0x7f})
+def is_binary(filename):
+    file_part = open(filename, 'rb').read(1024)
+    return bool(file_part.translate(None, textchars))
 
 def multithread_run(func_to_run, args_list):
     pool = Pool(processes=NUM_THREADS)
@@ -159,11 +223,14 @@ def get_uid(filename):
 
 def find_all_files(root):
     list_of_files=list()
+    list_of_subdirs=list()
     for path, subdirs, files in os.walk(root):
         for name in files:
             if not os.path.islink(os.path.join(path, name)):
                 list_of_files.append(os.path.join(path, name))
-    return list_of_files
+        for subdir in subdirs:
+            list_of_subdirs.append(os.path.join(path, subdir))
+    return list_of_files, list_of_subdirs
 
 def find_tmp_files(file_list):
     tmp_files=list()
