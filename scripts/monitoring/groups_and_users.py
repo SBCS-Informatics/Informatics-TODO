2c229ea6582284a2e6962ca5bd4ffe38b343740b
adrlar <adr.lar@me.com>
2017-08-29 17:24:02 +0100

    new users added and Irys group

--- scripts/monitoring/groups_and_users.py
+++ scripts/monitoring/groups_and_users.py
@@ -1,6 +1,6 @@
 
 groups = {  "Wurm":["btw586","btw749","btw928","btx076","btw947","btw877", "btw675", "btw788", "bt15686", "bt11112", "btx077", "btw826", "bt16005"],
-    "Buggs":["btw670", "btw633", "btw437", "eex057", "btw952"],
+    "Buggs":["btw670", "btw633", "btw437", "eex057", "btw952", "btx142"],
     "Chelala":["hfw515","hfw552","hfw487", "hfw456", "hfw425"],
     "Nichols":["mpx036","btw718", "btw678", "ugbt112"],
     "Rossiter":["btw532", "btw297", "btw799", "btw915", "bt03175"],
@@ -10,6 +10,7 @@ groups = {  "Wurm":["btw586","btw749","btw928","btx076","btw947","btw877", "btw6
     "Eiyce":["btx031"],
     "Lachlan":["btw774"],
     "Branco":["bt15130"],
+    "Irys":["btx167"],
     "Bessant":["btw796", "btx157"],
     "root/deamons":["root", "ganglia", "mysql", "sshd", "postfix", "rpcuser", "rpc", "dbus", "ntp", "nobody"]
     }
