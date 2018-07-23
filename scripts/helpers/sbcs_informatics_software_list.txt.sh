2ebfd83db6ab342652e5a52ad51b4bfcfa2c946d
adrlar <adr.lar@me.com>
2018-07-23 12:12:49 +0100

    hive mounting scripts for ref

---
+++ scripts/helpers/sbcs_informatics_software_list.txt.sh
@@ -0,0 +1,107 @@
+module load singularity/2.4.2
+
+#spades
+singularity pull docker://quay.io/biocontainers/spades:3.11.1--py35_zlib1.2.11_1
+
+#soapdenovo2
+singularity pull docker://quay.io/biocontainers/soapdenovo2:2.40--1
+
+#abyss
+singularity pull docker://quay.io/biocontainers/abyss:2.0.2--boost1.64_3
+#Alternative (most recent tag submission):	singularity pull docker://quay.io/biocontainers/abyss:1.5.2--boost1.61_4
+
+#mira
+singularity pull docker://quay.io/biocontainers/mira:4.9.6--0
+
+#canu
+singularity pull docker://quay.io/biocontainers/canu:1.7--pl5.22.0_0
+
+#bowtie2
+singularity pull docker://quay.io/biocontainers/bowtie2:2.3.4.1--py35pl5.22.0_0
+
+#bwa
+singularity pull docker://quay.io/biocontainers/bwa:0.7.17--pl5.22.0_2
+
+#star
+singularity pull docker://quay.io/biocontainers/star:2.5.4a--0
+#Alternative (most recent tag submission):	singularity pull docker://quay.io/biocontainers/star:2.4.0j--0
+#Alternative (name that was hard to compare):	singularity pull docker://quay.io/biocontainers/star:2.5.3a--0
+#Alternative (name that was hard to compare):	singularity pull docker://quay.io/biocontainers/star:2.5.2b--0
+#Alternative (name that was hard to compare):	singularity pull docker://quay.io/biocontainers/star:2.5.1b--0
+
+#fastqc
+singularity pull docker://quay.io/biocontainers/fastqc:0.11.7--pl5.22.0_0
+
+#trimmomatic
+singularity pull docker://quay.io/biocontainers/trimmomatic:0.36--5
+#Alternative (most recent tag submission):	singularity pull docker://quay.io/biocontainers/trimmomatic:0.33--0
+
+#khmer
+singularity pull docker://quay.io/biocontainers/khmer:2.1.2--py35_0
+
+#kmc
+singularity pull docker://quay.io/biocontainers/kmc:3.0.1--noasmlib_1
+
+#samtools
+singularity pull docker://quay.io/biocontainers/samtools:1.7--2
+#Alternative (most recent tag submission):	singularity pull docker://quay.io/biocontainers/samtools:1.2.rglab--0
+#Alternative (name that was hard to compare):	singularity pull docker://quay.io/biocontainers/samtools:latest
+
+#picard
+singularity pull docker://quay.io/biocontainers/picard:2.18.1--py35_0
+
+#seqtk
+singularity pull docker://quay.io/biocontainers/seqtk:r82--1
+
+#quast
+singularity pull docker://quay.io/biocontainers/quast:4.6.3--py27_boost1.64_1
+
+#bedtools
+singularity pull docker://quay.io/biocontainers/bedtools:2.27.1--1
+#Alternative (most recent tag submission):	singularity pull docker://quay.io/biocontainers/bedtools:2.26.0gx--1
+
+#genometools
+singularity pull docker://quay.io/biocontainers/genometools:1.2.1--py27_0
+
+#vcftools
+singularity pull docker://quay.io/biocontainers/vcftools:0.1.15--1
+#Alternative (most recent tag submission):	singularity pull docker://quay.io/biocontainers/vcftools:0.1.11--2
+
+#sra-tools
+singularity pull docker://quay.io/biocontainers/sra-tools:2.8.2--0
+
+#gatk
+singularity pull docker://quay.io/biocontainers/gatk:3.8--py35_2
+
+#gatk4
+singularity pull docker://quay.io/biocontainers/gatk4:4.0.3.0--0
+
+#qiime
+singularity pull docker://quay.io/biocontainers/qiime:1.9.1--np112py27_1
+
+#mothur
+singularity pull docker://quay.io/biocontainers/mothur:1.39.5--boost1.64_2
+
+#blast
+singularity pull docker://quay.io/biocontainers/blast:2.7.1--boost1.64_3
+
+#clustalw
+singularity pull docker://quay.io/biocontainers/clustalw:2.1--1
+
+#mafft
+singularity pull docker://quay.io/biocontainers/mafft:7.313--0
+
+#muscle
+singularity pull docker://quay.io/biocontainers/muscle:3.8.1551--2
+
+#raxml
+singularity pull docker://quay.io/biocontainers/raxml:8.2.10--0
+
+#mrbayes
+singularity pull docker://quay.io/biocontainers/mrbayes:3.2.6--0
+
+#maker
+singularity pull docker://quay.io/biocontainers/maker:2.31.9--3
+
+#augustus
+singularity pull docker://quay.io/biocontainers/augustus:3.3--boost1.64_1
